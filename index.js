c001b8711e22d1ca51fa87a93f21df8981dfa6c0
roger chat <mattrogerpelosi@gmail.com>
2021-08-09 13:19:20 -0400

    commit tree 4f0299e7761e369ea509d0b86fcfd8f75cdff197

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+let random = Math.ceil(Math.random()); 
+
+let num3 = 16;
+let num4 = 6;
+const mod = num3%num4;
+
+const max = (num1, num2, num3, num4, 20);
